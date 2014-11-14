984b69e111a9d114c22fbf786bb6109183900f52
Myles Bostwick <mbostwick@in-situ.com>
2014-11-14 15:37:12 -0700

    Get initial go at working plugin. Not working nicely with gulp.dest right now.

---
+++ README.md
@@ -0,0 +1,4 @@
+gulp-bower-prune
+================
+
+Get a list of files from bower dependencies according to their bower.json or exportOverrides in your bower.json.
---
+++ index.js
@@ -0,0 +1,88 @@
+// through2 is a thin wrapper around node transform streams
+var through = require('through2');
+var gutil = require('gulp-util');
+var minimatch = require('minimatch');
+var Path = require('path');
+var PluginError = gutil.PluginError;
+
+// consts
+const PLUGIN_NAME = 'gulp-bower-normalize';
+
+// Gets the component parts, package name, filename, ext
+function getComponents(file) {
+    console.log("Getting components");
+    var relativePath = file.relative;
+    console.log(relativePath);
+    var pathParts = Path.dirname(relativePath).split(Path.sep);
+    console.log(pathParts[0]);
+    return {
+        ext: Path.extname(relativePath).substr(1), // strip dot
+        filename: Path.basename(relativePath),
+        packageName: pathParts[0]
+    };
+}
+
+// plugin level function (dealing with files)
+function gulpBowerNormalize(options) {
+    var options = options || {},
+        bowerJson = options.bowerJson || "./bower.json",
+        overrides = {};
+
+    try {
+        overrides = require(bowerJson)["overrides"];
+    } catch(e) {
+        throw new PluginError(PLUGIN_NAME, "No bower.json at " + bowerJson + " or overrides invalid!");
+    }
+
+    // creating a stream through which each file will pass
+    var stream = through.obj(function(file, enc, cb) {
+        if (!file.isNull()) {
+            var components = getComponents(file),
+                type = components.ext,
+                pkgOverrides = null,
+                normalize = null;
+
+            // Check if there are overrides
+            if (components.packageName in overrides) {
+                pkgOverrides = overrides[components.packageName];
+                // Check if there are any normalize overrides
+                if ('normalize' in pkgOverrides) {
+                    normalize = pkgOverrides.normalize;
+                    // Loop through each type in normalize
+                    Object.keys(normalize).forEach(function(key) {
+                        var filter = normalize[key];
+
+                        // Check if it's a string and make it an array, just for ease of processing
+                        if (typeof filter === "string") {
+                            filter = [filter];
+                        }
+
+                        // Check each glob
+                        filter.map(function(glob) {
+                            // Potential bug here if multiple types match glob
+                            // Last one in wins right now, maybe throw error?
+                            if (minimatch(components.filename, glob)) {
+                                type = key;
+                            }
+                        });
+                    });
+                }
+            }
+
+            console.log("components", components.packageName, type, components.filename);
+            var path = Path.join(components.packageName, type, components.filename);
+            console.log("Setting path to " + path);
+            file.path = path
+        }
+
+        this.push(file);
+
+        return cb();
+    });
+
+    // returning the file stream
+    return stream;
+};
+
+// exporting the plugin main function
+module.exports = gulpBowerNormalize;
